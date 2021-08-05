8bc6c8292f7edb692db116889894141e66e445f4
Sarankumar S <saransekar94@gmail.com>
2021-08-05 10:53:38 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 resource "aws_instance" "devops"{
   ami           = "ami-06a0b4e3b7eb7a300"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "Devopstestnewec2"
