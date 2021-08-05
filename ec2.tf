039d7b334bd095105597a025e7e184f97d02691d
Sarankumar S <saransekar94@gmail.com>
2021-08-05 10:58:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,6 @@
-provider "aws"{
-  region = "ap-south-1"
-} 
-
 resource "aws_instance" "devops"{
   ami           = "ami-06a0b4e3b7eb7a300"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Devopstestnewec2"
