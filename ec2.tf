dc2755e4c062b766a3c84db99a539ecbaa2a1dba
Sarankumar S <saransekar94@gmail.com>
2021-08-09 17:34:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "devops"{
+/*resource "aws_instance" "devops"{
   ami           = "ami-06a0b4e3b7eb7a300"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "demoec2"
+    Name = "demoec21"
     }
-  }
+  }*/
