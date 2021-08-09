389ab37d4cbf5d5020234575364ad18c2521101e
Sarankumar S <saransekar94@gmail.com>
2021-08-09 17:37:53 +0530

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "dummydemobucket" {
+/*resource "aws_s3_bucket" "dummydemobucket" {
   bucket = "dummydemobucketjenkins"
   acl    = "private"
 
   tags = {
     Name        = "dummydemobucketjenkins"
   }
-}
+}*/
